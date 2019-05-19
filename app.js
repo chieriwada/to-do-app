98904c3fc488274e714acbe7568fec707e183a85
Chieri Wada <wada.chieri@gmail.com>
2019-05-18 16:23:19 -1000

    First commit for Checkpoint 6

--- app.js
+++ app.js
@@ -1,83 +1,46 @@
 function onReady() {
-  const ADD_TODO_FORM = document.getElementById('addToDoForm');
-  const NEW_TODO_TEXT = document.getElementById('newToDoText');
-  const TODO_LIST = document.getElementById('toDoList');
+  const toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
 
-  ADD_TODO_FORM.addEventListener('submit', (event) => {
-    event.preventDefault();
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if(!newToDoText.value) { return; }
 
-<<<<<<< HEAD
-    //get the text
-    let title = NEW_TODO_TEXT.value;
-
-    //create new li
-    let newLi = document.createElement('li');
-
-=======
-    //create new li
-    let newLi = document.createElement('li');
-
-    //create title element
-    let newLiTitle = document.createElement('text');
-
-    //style newLiTitle
-    newLiTitle.style.fontFamily = "Roboto","Helvetica","Arial","sans-serif";
-    newLiTitle.style.fontSize = "1em";
-    newLiTitle.style.padding = "0 1rem 0 1.5rem";
-
->>>>>>> assignment-5-DOM-part1
-    //create new input
-    let checkbox = document.createElement('input');
-
-    //set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-<<<<<<< HEAD
-    //set the title
-    newLi.textContent = title;
-=======
-    //get the text
-    let title = NEW_TODO_TEXT.value;
-
-    //create delete button
-    let deleteBtn = document.createElement('button');
-
-    //style deleteBtn
-    deleteBtn.innerHTML = '<img src="delete-icon.svg" />';
-    deleteBtn.style.border = "none";
-    deleteBtn.style.colorFill = "blue";
-    deleteBtn.style.cssFloat = "right";
-
-    //delete button action
-    deleteBtn.addEventListener('click', function(event) {
-      TODO_LIST.removeChild(this.parentElement);
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
     });
->>>>>>> assignment-5-DOM-part1
 
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
+    newToDoText.value = '';
 
-<<<<<<< HEAD
-=======
-    //set the title
-    newLiTitle.textContent = title;
+    renderTheUI();
+  };
 
-    //add title text
-    newLi.appendChild(newLiTitle);
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
 
-    //show delete button
-    newLi.appendChild(deleteBtn);
+    toDoList.textContent = '';
 
->>>>>>> assignment-5-DOM-part1
-    //attach the li to the ul
-    TODO_LIST.appendChild(newLi);
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
 
-    //empty the input
-    NEW_TODO_TEXT.value = "";
+      newLi.textContent = toDo.title;
 
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    createNewToDo();
   });
+
+  renderTheUI();
 }
 
-window.onload = function() {
+window.onLoad = function() {
   onReady();
 };
