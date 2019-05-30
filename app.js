8294f383f687bbf8c4ad0009c25060080567a2e3
Chieri Wada <wada.chieri@gmail.com>
2019-05-30 00:17:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,14 +8,28 @@ function onReady() {
     TODO_LIST.textContent = '';
 
     toDos.forEach(function(toDo){
+
+      //add new li
       const NEW_LI = document.createElement('li');
+
+      //add checkbox to li
       const CHECKBOX = document.createElement('input');
       CHECKBOX.type = "checkbox";
 
+      //add delete button to li
+      const DELETE_BTN = document.createElement('button');
+      DELETE_BTN.innerHTML = '<img src="delete-icon.svg" />';
+      DELETE_BTN.style.border = "none";
+      DELETE_BTN.style.colorFill = "blue";
+      DELETE_BTN.style.cssFloat = "right";
+
+      //text content of li is from todo list input
       NEW_LI.textContent = toDo.title;
 
+      //add new elements to new li
       TODO_LIST.appendChild(NEW_LI);
       NEW_LI.appendChild(CHECKBOX);
+      NEW_LI.appendChild(DELETE_BTN);
     });
   }
 
